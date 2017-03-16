fa46a4d4c157b8fa50cf6864b36bd59fa44c6e36
Akshit Patel <jodow@ymail.com>
2017-03-16 22:17:51 +0530

    Add all my files

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+/*Kartero App Configuration*/
+var krms_driver_config ={		
+	'ApiUrl':"http://app.jennyinfocom.com/api",					
+	'DialogDefaultTitle':"Kartero",	
+	'PushProjectID':"",	
+	'APIHasKey':""
+};
